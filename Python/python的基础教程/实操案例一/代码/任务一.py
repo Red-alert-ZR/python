d82f4207ac0b3b62e72d68a11c8f0cec0fe52687
Red-alert-ZR <99107924+Red-alert-ZR@users.noreply.github.com>
2022-08-31 11:50:34 +0800

    Update 任务一.py

--- Python/python的基础教程/实操案例一/代码/任务一.py
+++ Python/python的基础教程/实操案例一/代码/任务一.py
@@ -1,1 +1,8 @@
+'''一，使用print方式进行输出（输出的目的地是文件）'''
+fp=open('D:/IT社团/pythonProject7/test.txt', 'w')
+print('奋斗成就更好的你', file=fp)
+fp.close()
 
+'''第二种方式，使用文件读写操作'''
+with open('D:/IT社团/pythonProject7/test.txt', 'w') as file:
+    file.write('奋斗成就更好的你')
