784bd746ea5ad309de36a0230faea3a883e19aa9
Red-alert-ZR <99107924+Red-alert-ZR@users.noreply.github.com>
2022-09-15 23:59:33 +0800

    Update tank.py

--- Python/坦克大战项目实现/一种更好的思路/tank.py
+++ Python/坦克大战项目实现/一种更好的思路/tank.py
@@ -1,5 +1,5 @@
-import pygame, time, random
-from startGame import Startgame
+import pygame
+from sprites import *
 
 
 class TankGame():
@@ -25,7 +25,33 @@ class TankGame():
         pygame.mixer.init()
 
     def __create_sprite(self):
-        pass
+        self.walls = pygame.sprite.Group()
+        self.__draw_map()
+
+    def __draw_map(self):
+        """
+        绘制地图
+        """
+        for y in range(len(Startgame.MAP_ONE)):
+            for x in range(len(Startgame.MAP_ONE[y])):
+                if Startgame.MAP_ONE[y][x] == 0:
+                    continue
+                wall = Wall(Startgame.WALLS[Startgame.MAP_ONE[y][x]], self.screen)
+                wall.rect.x = x * Startgame.BOX_SIZE
+                wall.rect.y = y * Startgame.BOX_SIZE
+                if Startgame.MAP_ONE[y][x] == Startgame.RED_WALL:
+                    wall.type = Startgame.RED_WALL
+                elif Startgame.MAP_ONE[y][x] ==Startgame.IRON_WALL:
+                    wall.type = Startgame.IRON_WALL
+                elif Startgame.MAP_ONE[y][x] ==Startgame.WEED_WALL:
+                    wall.type = Startgame.WEED_WALL
+                elif Startgame.MAP_ONE[y][x] ==Startgame.SEA_WALL:
+                    wall.type = Startgame.SEA_WALL
+                elif Startgame.MAP_ONE[y][x] ==Startgame.BOSS_WALL:
+                    wall.type = Startgame.BOSS_WALL
+                    wall.life = 1
+                self.walls.add(wall)
+
 
     def __event_quit(self):
         for event in pygame.event.get():
@@ -34,6 +60,9 @@ class TankGame():
                 TankGame.__game_over()
                 print('游戏结束')
 
+    def __update_sprites(self):
+        self.walls.update()
+        self.walls.draw(self.screen)
 
 
     def run_game(self):
@@ -45,6 +74,8 @@ class TankGame():
             self.clock.tick(Startgame.FPS)
             # 2、事件监听
             self.__event_quit()
+            # 4、更新、绘制精灵、经理组
+            self.__update_sprites()
             # 5、更新显示
             pygame.display.update()
         self.__game_over()
