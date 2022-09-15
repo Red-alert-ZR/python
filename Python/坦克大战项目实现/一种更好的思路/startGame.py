cdabf8d353666e6855304f1610c1f09ff2b7074d
Red-alert-ZR <99107924+Red-alert-ZR@users.noreply.github.com>
2022-09-15 23:59:47 +0800

    Update startGame.py

--- Python/坦克大战项目实现/一种更好的思路/startGame.py
+++ Python/坦克大战项目实现/一种更好的思路/startGame.py
@@ -9,6 +9,55 @@ class Startgame:
     BOX_SIZE = 50
     BOX_RECT = Rect(0, 0, BOX_SIZE, BOX_SIZE)
     SCREEN_RECT = Rect(0, 0, BOX_SIZE * 20, BOX_SIZE * 15)
-    SCREEN_COLOR = (51, 76, 117)
+    SCREEN_COLOR = (0, 0, 0)
+
+    # 通用变量
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+
+    # 地图
+    MAP_ONE = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 3, 3, 1, 1, 2, 1, 1, 3, 3, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 3, 3, 1, 1, 2, 1, 1, 3, 3, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 4, 4, 1, 1, 2, 1, 1, 4, 4, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 4, 4, 1, 1, 2, 1, 1, 4, 4, 1, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 0, 1, 4, 4, 1, 1, 2, 1, 1, 4, 4, 1, 0, 0, 1, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+        [1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, ],
+        [0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, ],
+        [0, 0, 1, 3, 3, 3, 1, 0, 0, 1, 1, 1, 0, 0, 1, 3, 3, 3, 1, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    ]
+
+    # 音频
+    BOOM_MUSIC ="TankWar-master/music/bang.wav"
+    START_MUSIC = "TankWar-master/music/start.wav"
+    FIRE_MUSIC = "TankWar-master/music/fire.wav"
+    HIT_MUSIC = "TankWar-master/music/hit.wav"
 
 
+
+    # 0表示空白、1表示红墙、2表示铁墙、3表示草、4表示海、5表示老巢
+    RED_WALL = 1
+    IRON_WALL = 2
+    WEED_WALL = 3
+    SEA_WALL = 4
+    BOSS_WALL = 5
+    WALLS = [
+        f"resources/images/walls/{file}" for file in os.listdir("resources/images/walls")
+    ]
+
+
+
+
+    # 爆炸的图片
+    BOOMS = [
+        "resources/images/boom/" + file for file in os.listdir("resources/images/boom")
+    ]
