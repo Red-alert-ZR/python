bd0c6a7b107bc69d044cc16b27ff9e4f4f1659ff
Red-alert-ZR <99107924+Red-alert-ZR@users.noreply.github.com>
2022-09-17 23:02:27 +0800

    Update startGame.py

--- Python/坦克大战项目实现/一种更好的思路/startGame.py
+++ Python/坦克大战项目实现/一种更好的思路/startGame.py
@@ -1,4 +1,5 @@
 from pygame import Rect
+import random
 import os
 
 class Startgame:
@@ -28,7 +29,7 @@ class Startgame:
         [0, 0, 1, 0, 0, 1, 4, 4, 1, 1, 0, 1, 1, 4, 4, 1, 0, 0, 1, 0, ],
         [0, 0, 1, 0, 0, 1, 4, 4, 1, 1, 2, 1, 1, 4, 4, 1, 0, 0, 1, 0, ],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+        [0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, ],
         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ],
         [0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, ],
         [0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, ],
@@ -42,7 +43,37 @@ class Startgame:
     FIRE_MUSIC = "TankWar-master/music/fire.wav"
     HIT_MUSIC = "TankWar-master/music/hit.wav"
 
+    # 坦克类型
+    HERO = 0
+    ENEMY = 1
 
+    # 我方坦克
+    HERO_IMAGE_NAME = "resources/images/hero/hero1U.gif"
+    HERO_IMAGES ={
+        LEFT : "resources/images/hero/hero1L.gif",
+        RIGHT : "resources/images/hero/hero1R.gif",
+        UP : "resources/images/hero/hero1U.gif",
+        DOWN : "resources/images/hero/hero1D.gif"
+    }
+    HERO_SPEED = 2
+
+    # 我方老巢
+    BOSS_IMAGE = ""
+
+    # 敌方坦克
+    ENEMY_IMAGES ={
+        LEFT : "/resources/images/A_enemy/enemyL.png",
+        RIGHT : "/resources/images/A_enemy/enemyR.png",
+        UP : "/resources/images/A_enemy/enemyU.png",
+        DOWN : "/resources/images/A_enemy/enemyD.png"
+    }
+    ENEMY_COUNT = 5
+    ENEMY_SPEED = random.uniform(1.0, 2.5)
+
+    # 子弹
+    BULLET_IMAGE_NAME = "/resources/images/bullet/bullet.png"
+    BULLET_RECT = Rect(0, 0, 5, 5)
+    BULLET_SPEED = 5
 
     # 0表示空白、1表示红墙、2表示铁墙、3表示草、4表示海、5表示老巢
     RED_WALL = 1
